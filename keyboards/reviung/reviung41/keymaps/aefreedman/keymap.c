c1eb606a2f7ed2198e841b2e22067c7e6e611958
aefreedman <aaron@secretcrush.net>
2026-01-25 15:44:43 -0500

    fixed edgecase where using control and shift tapdance keys can lead to shift key triggering a onetap state

--- keyboards/reviung/reviung41/keymaps/aefreedman/keymap.c
+++ keyboards/reviung/reviung41/keymaps/aefreedman/keymap.c
@@ -149,20 +149,19 @@ XXXXXXX,   XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,   XXXXXXX,            QK_BOOT, 
 // Determine the tapdance state to return
 td_state_t cur_dance(qk_tap_dance_state_t *state) {
     if (state->count == 1) {
-        if (state->interrupted || !state->pressed) return TD_SINGLE_TAP;
-        // Key has not been interrupted, but the key is still held. Means you want to send a 'HOLD'.
-        else return TD_SINGLE_HOLD;
+        if (state->pressed) return TD_SINGLE_HOLD;
+        return TD_SINGLE_TAP;
     } else if (state->count == 2) {
-        if (state->interrupted || !state->pressed) return TD_DOUBLE_TAP;
-        else return TD_DOUBLE_HOLD;
+        if (state->pressed) return TD_DOUBLE_HOLD;
+        return TD_DOUBLE_TAP;
     }
 
     // Assumes no one is trying to type the same letter three times (at least not quickly).
     // If your tap dance key is 'KC_W', and you want to type "www." quickly - then you will need to add
     // an exception here to return a 'TD_TRIPLE_SINGLE_TAP', and define that enum just like 'TD_DOUBLE_SINGLE_TAP'
     if (state->count == 3) {
-        if (state->interrupted || !state->pressed) return TD_TRIPLE_TAP;
-        else return TD_TRIPLE_HOLD;
+        if (state->pressed) return TD_TRIPLE_HOLD;
+        return TD_TRIPLE_TAP;
     } else return TD_UNKNOWN;
 }
 
